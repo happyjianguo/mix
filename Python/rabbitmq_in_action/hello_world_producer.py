6cf722a50abcb4170a97f7e8e4a47bf078e7e87e
joshua <517081935@qq.com>
2016-09-02 17:54:04 +0800

    rabbitmq basic usage

---
+++ Python/rabbitmq_in_action/hello_world_producer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import pika, sys
+
+credentials = pika.PlainCredentials("guest", "guest")
+conn_params = pika.ConnectionParameters("localhost", credentials=credentials)
+conn_broker = pika.BlockingConnection(conn_params)
+channel = conn_broker.channel()
+channel.exchange_declare(exchange="hello-exchange", type="direct")
+msg = sys.argv[1]
+msg_props = pika.BasicProperties()
+msg_props.content_type = "text/plain"
+
+channel.basic_publish(body=msg, exchange="hello-exchange", properties=msg_props, routing_key="hola")
