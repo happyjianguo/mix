c5635716580a7dab4c24abb2e5de8bdae2b613cf
joshua <517081935@qq.com>
2016-09-29 17:23:45 +0800

    senior template

--- Python/django_study/zqxt_tmpl/learn/views.py
+++ Python/django_study/zqxt_tmpl/learn/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home.html')
+    string = u"hello, world"
+    TutorialList = ['html', 'css', 'jquery', 'python', 'django']
+    info_dict = {'name': 'fanteathy'}
+    return render(request, 'home.html', {'string': string, 'TutorialList': TutorialList, 'info_dict': info_dict})
