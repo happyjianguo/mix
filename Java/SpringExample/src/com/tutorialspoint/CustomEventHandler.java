1d908690c642120521b379f307e8e484bfd16231
joshua <517081935@qq.com>
2016-11-20 13:24:05 +0800

    rm spring demo

--- Java/SpringExample/src/com/tutorialspoint/CustomEventHandler.java
+++
@@ -1,8 +0,0 @@
-package com.tutorialspoint;
-import org.springframework.context.ApplicationListener;
-public class CustomEventHandler
-        implements ApplicationListener<CustomEvent>{
-    public void onApplicationEvent(CustomEvent event) {
-        System.out.println(event.toString());
-    }
-}
--- Java/spring-1/src/com/atguigu/spring/beans/test/Moveable.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.spring.beans.test;
-
-/**
- * Created by joshua on 16/6/4.
- */
-public interface Moveable {
-    void run();
-}
