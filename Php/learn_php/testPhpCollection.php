6fa87fb467df387cb92a5d0bad4843b5ea4e7fb5
fanteathy <517081935@qq.com>
2017-01-09 14:46:32 +0800

    test phpcollection

---
+++ Php/learn_php/testPhpCollection.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * test phpcollection
+ * User: joshua
+ * Date: 17/1/9
+ * Time: 14:34
+ */
+
+include_once("vendor/autoload.php");
+
+use PhpCollection\Sequence;
+use PhpCollection\Map;
+
+$seq = new Sequence([0, 2, 3, 2]);
+var_dump($seq->get(2)); // int(3)
+var_dump($seq->all()); // [0, 2, 3, 2]
+
+// Read Operations
+$map = new Map(['name' => 'fanteathy', 'height' => '170']);
+var_dump($map->get('name'));
+var_dump($map->get('height'));
