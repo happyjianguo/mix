7c3b0b4b4b1f1861238cfe8ccea7c92d08542c15
joshua <517081935@qq.com>
2017-01-26 23:20:51 +0800

    test commit name

--- Php/learn_php/testPhpCollection.php
+++ Php/learn_php/testPhpCollection.php
@@ -16,6 +16,6 @@ var_dump($seq->get(2)); // int(3)
 var_dump($seq->all()); // [0, 2, 3, 2]
 
 // Read Operations
-$map = new Map(['name' => 'fanteathy', 'height' => '170']);
+$map = new Map(['name' => 'joshua', 'height' => '170']);
 var_dump($map->get('name'));
 var_dump($map->get('height'));
