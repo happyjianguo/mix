1803223340b3c0e849b154ba64c3da38bef6c57b
Joshua <517081935@qq.com>
2017-02-03 17:12:17 +0800

    optimize formatter

--- Java/learn_java/src/main/java/test/netty/EchoClient.java
+++ Java/learn_java/src/main/java/test/netty/EchoClient.java
@@ -1,44 +1,44 @@
 package test.netty;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-
-import java.net.InetSocketAddress;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 public class EchoClient {
 
-    private final String host;
-    private final int port;
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "10000"));
+    static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
 
-    public EchoClient(String host, int port) {
-        this.host = host;
-        this.port = port;
-    }
+    public static void main(String[] args) throws Exception {
 
-    public void start() throws Exception {
+        // Configure the client.
         EventLoopGroup group = new NioEventLoopGroup();
         try {
             Bootstrap b = new Bootstrap();
-            b.group(group).channel(NioSocketChannel.class).remoteAddress(new InetSocketAddress(host, port))
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
                     .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new EchoClientHandler());
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast("decoder", new StringDecoder());
+                            p.addLast("encoder", new StringEncoder());
+                            p.addLast(new EchoClientHandler());
                         }
                     });
-            ChannelFuture f = b.connect().sync();
-            f.channel().closeFuture().sync();
+
+            ChannelFuture future = b.connect(HOST, PORT).sync();
+            future.channel().writeAndFlush("Hello Netty Server ,I am a common client");
+            future.channel().closeFuture().sync();
         } finally {
-            group.shutdownGracefully().sync();
+            group.shutdownGracefully();
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        new EchoClient("localhost", 65535).start();
-    }
 }
--- Java/learn_java/src/main/java/test/netty/EchoServerHandler.java
+++ Java/learn_java/src/main/java/test/netty/EchoServerHandler.java
@@ -1,21 +1,17 @@
 package test.netty;
 
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("Server received: " + msg);
-        ctx.write(msg);
-    }
 
     @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("server channelRead..");
+        System.out.println(ctx.channel().remoteAddress() + "->Server :" + msg.toString());
+        ctx.write("server write" + msg);
+        ctx.flush();
     }
 
     @Override
@@ -25,3 +21,4 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     }
 
 }
+
