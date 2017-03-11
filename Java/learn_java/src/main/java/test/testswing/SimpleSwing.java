961d23d2a1bd2acb91e71199e1cc7cf31645543c
Joshua <517081935@qq.com>
2017-03-11 18:42:28 +0800

    java swing test

---
+++ Java/learn_java/src/main/java/test/testswing/SimpleSwing.java
@@ -0,0 +1,19 @@
+package test.testswing;
+
+import javax.swing.*;
+
+/**
+ * Created by joshua on 17/3/11.
+ */
+public class SimpleSwing {
+
+    public static void main(String[] args) {
+
+        JFrame jf = new JFrame();
+        jf.setTitle("Hello");
+        jf.setSize(300, 300);
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(3);
+        jf.setVisible(true);
+    }
+}
