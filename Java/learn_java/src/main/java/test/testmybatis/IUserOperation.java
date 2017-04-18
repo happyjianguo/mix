b7289e0537b908601efd31339a614cb55a6f8b0b
Joshua <517081935@qq.com>
2017-04-18 18:06:09 +0800

    operate mybatis with interface

---
+++ Java/learn_java/src/main/java/test/testmybatis/IUserOperation.java
@@ -0,0 +1,9 @@
+package test.testmybatis;
+
+/**
+ * Created by joshua on 17/4/18.
+ */
+public interface IUserOperation {
+
+    UserModel selectUserByID(int id);
+}
---
+++ Java/learn_java/src/main/java/test/testmybatis/TestTwo.java
@@ -0,0 +1,42 @@
+package test.testmybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.Reader;
+
+/**
+ * Created by joshua on 17/4/18.
+ */
+public class TestTwo {
+
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Reader reader;
+
+    static {
+        try {
+            reader = Resources.getResourceAsReader("testmybatis/configuration.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSession() {
+        return sqlSessionFactory;
+    }
+
+    public static void main(String[] args) {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            IUserOperation userOperation = session.getMapper(IUserOperation.class);
+            UserModel user = userOperation.selectUserByID(1);
+            System.out.println(user.getUserAddress());
+            System.out.println(user.getUserName());
+        } finally {
+            session.close();
+        }
+    }
+}
