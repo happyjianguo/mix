658d704631d3727883fdc09ec7b5f3f180b93a9b
Joshua <517081935@qq.com>
2017-04-19 16:23:28 +0800

    hibernate test

---
+++ Java/learn_java/src/main/java/test/testhibernate2/ManageEmployee.java
@@ -0,0 +1,48 @@
+package test.testhibernate2;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by joshua on 17/4/19.
+ */
+public class ManageEmployee {
+
+    private static SessionFactory factory;
+
+    public static void main(String[] args) {
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object. " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        ManageEmployee me = new ManageEmployee();
+        Integer empID1 = me.addEmployee("Zara", "Ali", 1000);
+        Integer empID2 = me.addEmployee("Daisy", "Das", 5000);
+        Integer empID3 = me.addEmployee("John", "Paul", 10000);
+
+    }
+
+    public Integer addEmployee(String fname, String lname, int salary) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer employeeID = null;
+        try {
+            tx = session.beginTransaction();
+            Employee employee = new Employee(fname, lname, salary);
+            employeeID = (Integer) session.save(employee);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return employeeID;
+    }
+}
