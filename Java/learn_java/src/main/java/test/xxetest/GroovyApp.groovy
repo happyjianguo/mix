2f8c9de81bfed60934518b71fdd269e3134c259c
chuanshi.hzh <chuanshi.hzh@alibaba-inc.com>
2017-05-10 20:26:09 +0800

    groovy hello world

---
+++ Java/learn_java/src/main/java/test/xxetest/GroovyApp.groovy
@@ -0,0 +1,13 @@
+package test.xxetest
+
+/**
+ * Created by chuanshi.hzh on 2017/5/10.
+ * @author chuanshi.hzh
+ * @date 2017/05/10
+ */
+class GroovyApp {
+
+    void hello() {
+        println("hello, world")
+    }
+}
---
+++ Java/learn_java/src/main/java/test/xxetest/main.groovy
@@ -0,0 +1,10 @@
+package test.xxetest
+
+/**
+ * Created by chuanshi.hzh on 2017/5/10.
+ * @author chuanshi.hzh
+ * @date 2017/05/10
+ */
+
+app = new GroovyApp()
+app.hello()
