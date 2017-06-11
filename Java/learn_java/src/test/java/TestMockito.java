5866b5f3fc94ee124c3bd95bdcbd34d4aedc02f6
chuanshi.hzh <chuanshi.hzh@alibaba-inc.com>
2017-06-11 14:37:51 +0800

    mockito test

--- Java/learn_java/src/test/java/TestMockito.java
+++ Java/learn_java/src/test/java/TestMockito.java
@@ -1,7 +1,10 @@
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Assert.*;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -11,6 +14,7 @@ import static org.mockito.Mockito.when;
  * Created by chuanshi.hzh on 2017/6/4.
  *
  * @author chuanshi.hzh
+ * @link http://blog.csdn.net/sdyy321/article/details/38757135/
  * @date 2017/06/04
  */
 public class TestMockito {
@@ -26,18 +30,11 @@ public class TestMockito {
     }
 
     @Test
-    public void testStubbing() {
-        LinkedList mockedList = mock(LinkedList.class);
-        when(mockedList.get(0)).thenReturn("first");
-        when(mockedList.get(1)).thenThrow(new RuntimeException());
-
-        //following prints "first"
-        System.out.println(mockedList.get(0));
-
-        //following throws runtime exception
-        //System.out.println(mockedList.get(1));
-
-        verify(mockedList).get(0);
+    public void when_thenReturn() {
+        Iterator iterator = mock(Iterator.class);
+        when(iterator.next()).thenReturn("hello").thenReturn("world");
+        String result = iterator.next() + " " + iterator.next() + " " + iterator.next();
+        Assert.assertEquals("hello world world", result);
     }
 
 }
