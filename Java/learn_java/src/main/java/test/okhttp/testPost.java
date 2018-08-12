85a27e35b81a0465e4493b407581e5e726565fe8
chuanshi <chuanshi@pinduoduo.com>
2018-08-12 21:09:47 +0800

    test okhttp

--- Java/learn_java/src/main/java/test/okhttp/testPost.java
+++ Java/learn_java/src/main/java/test/okhttp/testPost.java
@@ -1,18 +1,27 @@
 package test.okhttp;
 
-import okhttp3.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import java.io.IOException;
 
 /**
  * okhttp post
  */
-public class testPost {
+public class TestPost {
 
     public static final MediaType MEDIA_TYPE_MARKDOWN = MediaType.parse("text/x-markdown; charset=utf-8");
     public final static OkHttpClient client = new OkHttpClient();
 
     public static void main(String[] args) {
+        System.out.println(new TestPost().getChronos());
+
+    }
+
+    public void testPost() {
         String postBody = ""
                 + "Releases\n"
                 + "--------\n"
@@ -31,6 +40,28 @@ public class testPost {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public String getChronos() {
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"biz_prefix\":\"cl\",\"db_table_index\":\"xxxxxx\"}");
+        Request request = new Request.Builder()
+                .url("http://172.22.252.8:21012/getId")
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("App-Key", "test")
+                .addHeader("sign", "f9c0aef5151a4c7e9f7566de0319476ff5e1577a")
+                .addHeader("Cache-Control", "no-cache")
+                .addHeader("Postman-Token", "a34ea726-4201-43a4-83e6-6c58ce8bef5f")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
